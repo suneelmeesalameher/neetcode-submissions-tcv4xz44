bbd42658ec5f5ea4d9e8a232020718431e0d278a
Meher Suneel <38861451+suneelmeesalameher@users.noreply.github.com>
2026-07-30 18:58:59 -0500

    Add: anagram-groups - submission-9

---
+++ Data Structures & Algorithms/anagram-groups/submission-9.py
@@ -0,0 +1,32 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        mydir=dict(list)
+        res=[]
+        #sort the words
+        for word in strs:
+            key= "".join(sorted(word))
+            mydir[key].append(word)
+        
+        for key, value in mydir.items():
+            res.append(value)
+        return sorted(res)
+
+
+        # Raw DOG method
+        # directoryofdir={}
+        # overallDirect=[]
+
+        # for word in strs:
+        #     mydir={}
+        #     for w in word:
+        #         mydir[w]= mydir.get(w,0)+1
+        #     if tuple(sorted(mydir.items())) in directoryofdir:
+        #         directoryofdir[tuple(sorted(mydir.items()))].append(word)
+        #     else:
+        #         directoryofdir[tuple(sorted(mydir.items()))]=[word]
+        
+        # for key, value in directoryofdir.items():
+        #     overallDirect.append(value)
+    
+        # return sorted(overallDirect)
+
